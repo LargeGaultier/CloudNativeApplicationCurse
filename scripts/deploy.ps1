63ac20cb8dd70877e073afcffd000082e3072368
abbouizd <dylanabbouize@gmail.com>
2026-01-14 14:58:51 +0100

    fix: make deploy ci use a default .env

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,18 +1,17 @@
-# scripts/deploy.ps1
 param(
   [string]$Sha
 )
 
 Write-Host "=== Déploiement local avec SHA $Sha ==="
 
-# 1. Arrêt propre des conteneurs (sans supprimer les volumes)
-docker compose down
+# 1. Arrêt propre des conteneurs (en prenant les variables du .env généré)
+docker compose --env-file .env down
 
 # 2. Pull des images depuis GHCR
 docker pull ghcr.io/dylanabz/cloudnative-backend:$Sha
 docker pull ghcr.io/dylanabz/cloudnative-frontend:$Sha
 
 # 3. Relance de l'environnement complet
-docker compose up -d
+docker compose --env-file .env up -d
 
 Write-Host "=== Déploiement terminé, services relancés ==="
